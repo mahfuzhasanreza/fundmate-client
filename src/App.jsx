626bbcb732be8fdb3d0b74e0fe629ec8b5e81a3e
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2025-11-28 12:02:13 +0600

    added loan request page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import LandingPage from './pages/LandingPage'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
+import LoanRequest from './pages/LoanRequest'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/loan-request" element={<LoanRequest />} />
           <Route path="/" element={
             <>
               <Navbar />
